6ddfd436c29bb15aa34ea0430b8743b974c9907b
Jethro Carr <jethro.carr@jethrocarr.com>
2016-09-17 21:59:25 +1200

    Structured Dovecot config to be the same as CentOS

--- manifests/dovecot.pp
+++ manifests/dovecot.pp
@@ -16,33 +16,114 @@ class mail::dovecot (
   ensure_packages([$packages_dovecot])
 
   service { $service_dovecot:
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => Package[ $packages_dovecot ],
   }
 
 
-  # Deploy our configuration into the OS-provided configuration structure.
-  # TODO: This will be RHEL-specific.
-  file { '/etc/dovecot/conf.d/01-puppet.conf':
-    ensure  => present,
-    content => template('mail/dovecot.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  # We take the RedHat-style approach and deploy our configuration into the
+  # provided /etc/dovecot/conf.d/ directory. Whilst we could create a cutdown
+  # "one file" style configuration file and purge all the files we don't want,
+  # this would put us at risk of the config breaking next time Dovecot gets
+  # updated by the OS. By maintaining the same structural convention, the
+  # config get respected by the package manager.
+
+  file {'/etc/dovecot/conf.d/10-auth.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-auth.conf.erb'),
     require => Package[ $packages_dovecot ],
     notify  => Service[ $service_dovecot ],
   }
-
-
-  # On RedHat, we need to remove the default SSL configuration that conflicts
-  # with the SSL configuration we've loaded in via our module.
-  if ($::osfamily == 'RedHat') {
-
-    file {'/etc/dovecot/conf.d/10-ssl.conf':
-      ensure  => 'file',
-      content => '# Disabled by Puppet',
-      notify  => Service[ $service_dovecot ],
-     }
+  file {'/etc/dovecot/conf.d/10-director.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-director.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/10-logging.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-logging.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/10-mail.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-mail.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/10-master.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-master.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/10-ssl.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/10-ssl.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/15-lda.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/15-lda.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/15-mailboxes.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/15-mailboxes.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/20-imap.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/20-imap.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/20-managesieve.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/20-managesieve.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/20-pop3.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/20-pop3.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/90-acl.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/90-acl.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/90-plugin.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/90-plugin.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/90-quota.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/90-quota.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/90-sieve-extprograms.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/90-sieve-extprograms.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
+  }
+  file {'/etc/dovecot/conf.d/90-sieve.conf':
+    ensure  => 'file',
+    content => template('mail/dovecot/90-sieve.conf.erb'),
+    require => Package[ $packages_dovecot ],
+    notify  => Service[ $service_dovecot ],
   }
 
 
